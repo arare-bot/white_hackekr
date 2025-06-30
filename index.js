4d7235b05e9c78144129c10206c40fefb80f0203
arare <lianguuchuan69@gmail.com>
2025-06-30 14:58:48 +0900

    Create cookie stage.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const path = require('path');
 const express = require('express');
 const flash = require('connect-flash')
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const sessionOptions = {secret: 'kmkcadc', resave: false, saveUninitialized: false};
+app.use(cookieParser('mysecret'));
 app.use(session(sessionOptions));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
@@ -25,7 +27,7 @@ app.get('/problems/:num', (req, res) =>{
 app.post('/problems/:num', (req, res) =>{
     const { ans } = req.body;
     const { num } = req.params;
-    if (ans === 'aaa') {
+    if (ans === 'namine') {
         res.redirect(`/problems/${String(num)}/editorial`);
     } else {
         req.flash('failed', '不正解です');
@@ -36,6 +38,24 @@ app.post('/problems/:num', (req, res) =>{
 app.get('/problems/:num/editorial', (req, res) =>{
     const { num } = req.params;
     res.render('problems/editorial', { num });
+}) 
+
+app.get('/cookie', (req, res) =>{
+    if(!req.cookies.admin) {
+        res.cookie('admin', 'false');
+        res.cookie('darkmode', 'false');
+        res.cookie('country', 'jp');
+    }
+    const { admin, darkmode, country } = req.cookies;
+    console.log('admin:', req.cookies);
+
+    if(admin == 'true') {
+        // console.log('tureです')
+        res.render('target-site/cookie/admin', {darkmode, country});
+    } else {
+        // console.log('falseです')
+        res.render('target-site/cookie/user', {darkmode, country});
+    }
 }) 
 
 app.listen(3000, () => {
